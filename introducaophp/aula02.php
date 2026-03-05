81b72500b8e9b84bbddfac36c24c7bc2be9969cd
luis gustavo <luisgustavomarilia52@gmail.com>
2026-03-05 20:31:15 -0300

    first commit

---
+++ introducaophp/aula02.php
@@ -0,0 +1,38 @@
+<?php
+
+// TODAS AS VARIAVEIS
+//~~~~~~~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~~~~~~
+     //variavéis    
+        $fruta = "banana";
+       
+    //Variável do tipo Array
+        $listaFrutas = ["Banana","Maça","Pera",10];
+
+   // COMANDOS     
+//~~~~~~~~~~~~~~~~~~~~~~~~||~~~~~~~~~~~~~~~~~~~~~~~~
+        echo $listaFrutas[2];
+    
+    //print_r = não retorna um tipo
+        print_r($listaFrutas);
+        echo "<hr>";
+    
+    //var_dump = mostra qauntidade de caractere, posição,nome.    
+        var_dump($listaFrutas);
+        echo"<hr>";
+
+        echo $listaFrutas;
+
+//  echo date("d/m/Y H:i:s");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
