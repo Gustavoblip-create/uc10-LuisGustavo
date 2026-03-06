fdb6380b57c4dd971a4afe5a092bf3d1bae9c77f
luis gustavo <luisgustavomarilia52@gmail.com>
2026-03-06 19:15:41 -0300

    correção

--- introducaophp/09conexaomysql.php
+++ introducaophp/09conexaomysql.php
@@ -1,26 +1,22 @@
 <?php 
-    $servidor = 'localhost';   
-    $usuario = 'root';
-    $senha = '';
-    $banco = 'loja';
+$servidor = 'localhost';   
+$usuario = 'root';
+$senha = '';
+$banco = 'loja';
 
-    $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+$conexao = new mysqli($servidor,$usuario,$senha,$banco);
 
-    if($conexao ->connect_error){
+if($conexao->connect_error){
+    die('Erro na conexão');
+}
 
-        die('Erro na conexão');
+$sql = "SELECT id, nome, preco, estoque FROM produtos";
+$resultado = $conexao->query($sql);
+
+if($resultado->num_rows > 0) {
+    while ($linha = $resultado->fetch_assoc()) {
+
+        echo "<p>Código: {$linha['id']} Produto: {$linha['nome']} Preço: {$linha['preco']} Estoque: {$linha['estoque']}</p>";
     }
-
-    // echo 'Conexão realizada com sucesso!!';
-
-    $sql = "select id, nome, preco, estoque from produtos";
-    $resultado = $conexao->query($sql);
-
-    if($resultado->num_rows > 0) {
-            while ($linha = $resultado->fetch_assoc()) {
-
-                echo "<p> Código: $linha [id] Produto: $linha [nome] preço  $linha [preco] Estoque : $linha [estoque]";
-            }
-
-    }
+}
 ?>
